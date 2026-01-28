df48ea217889e7311aa136fa134aaa29d6059c3c
Justine <justineroure0610@gmail.com>
2026-01-28 10:14:33 +0100

    Ajout du script de Justine

---
+++ fonction/partie_justine.sh
@@ -0,0 +1,12 @@
+source ./TP_Final_Bash_Serveur_Autonome_2_main.sh
+
+#Journalisation et robustesse
+
+## Fonction de journalisation standardisée
+
+cd /var/log
+echo= "=== Création d'un répertoire maintenance ==="
+mkdir maintenance ## Création du répertoire de log nommé "maintenance"
+cd maintenance
+
+nano maintenance_$(date+%Y%m%d_%H%M%S).log
