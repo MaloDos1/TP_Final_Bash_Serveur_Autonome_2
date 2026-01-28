36f78f33b8edd28c753b48ee71e9d8688954cb3e
Justine <justineroure0610@gmail.com>
2026-01-28 12:23:55 +0100

    Ajout du code de journalisation pour Justine

---
+++ fonction/partie_justine.sh
@@ -0,0 +1,12 @@
+source ./serveur_autonome.sh
+
+#Journalisation et robustesse
+
+## Fonction de journalisation standardisée
+
+cd /var/log
+echo "=== Création d'un répertoire maintenance ==="
+mkdir maintenance ## Création du répertoire de log nommé "maintenance"
+cd maintenance
+
+echo "Début de la maintenance" >> "maintenance_$(date+%Y%m%d_%H%M%S).log"
